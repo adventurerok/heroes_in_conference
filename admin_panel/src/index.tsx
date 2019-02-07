4e3725096bdf754a44a5214a9804dc35a4c2cbf9
paul <pauldurbz@gmail.com>
2019-02-07 13:20:02 +0000

    Install react-router

--- admin_panel/src/index.tsx
+++ admin_panel/src/index.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App />,
+    <Router>
+        <App />
+    </Router>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
